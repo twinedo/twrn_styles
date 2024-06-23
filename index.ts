b76c69d1b9de871a6605b4964388ff433c712b3d
Twin Edo <twinedo.dev@gmail.com>
2024-06-24 01:15:37 +0700

    export all

---
+++ index.ts
@@ -0,0 +1,4 @@
+export * from 'styles/colors';
+export * from 'styles/fontFamily';
+export * from 'styles/globalStyles';
+export * from 'styles/sizes';
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,18 +1,39 @@
-export const PRIMARY = '#00438C';
-export const SECONDARY = '#7B0BFF';
-export const BLACK = '#121212';
-export const WHITE = '#FFFFFF';
-export const RED = '#F31818';
-export const GREYB11 = '#B1B1B1';
-export const GREY7F = '#7f7e7c';
-export const GREYD9 = '#D9D9D9';
-export const GREEN1 = '#83BD75';
-export const GREEN2 = '#009536';
-export const SOFTGREEN = '#C3FFD9';
-export const BLUE = '#2A94FA';
-export const BLUE2 = '#189DE4';
-export const BLUE3 = '#165091';
-export const BLUE4 = '#1F3E7C';
-export const ORANGE = '#FFA000';
-export const YELLOW = '#FED500';
-export const PINK = 'pink';
+const PRIMARY = '#00438C';
+const SECONDARY = '#7B0BFF';
+const BLACK = '#121212';
+const WHITE = '#FFFFFF';
+const RED = '#F31818';
+const GREYB11 = '#B1B1B1';
+const GREY7F = '#7f7e7c';
+const GREYD9 = '#D9D9D9';
+const GREEN1 = '#83BD75';
+const GREEN2 = '#009536';
+const SOFTGREEN = '#C3FFD9';
+const BLUE = '#2A94FA';
+const BLUE2 = '#189DE4';
+const BLUE3 = '#165091';
+const BLUE4 = '#1F3E7C';
+const ORANGE = '#FFA000';
+const YELLOW = '#FED500';
+const PINK = 'pink';
+
+export {
+  PRIMARY,
+  SECONDARY,
+  BLACK,
+  WHITE,
+  RED,
+  GREEN1,
+  GREEN2,
+  GREYB11,
+  GREY7F,
+  GREYD9,
+  SOFTGREEN,
+  BLUE,
+  BLUE2,
+  BLUE3,
+  BLUE4,
+  ORANGE,
+  YELLOW,
+  PINK,
+};
