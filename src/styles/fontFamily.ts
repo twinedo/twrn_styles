006629c3418030ee469c9766d8429cb69fdefe92
Twin Edo <twinedo.dev@gmail.com>
2024-06-23 23:12:15 +0700

    add colors, fontFamily, globalStyles, sizes, config

---
+++ src/styles/fontFamily.ts
@@ -0,0 +1,10 @@
+const fontFamily = {
+  black: 'Roboto-Black',
+  bold: 'Roboto-Bold',
+  light: 'Roboto-Light',
+  medium: 'Roboto-Medium',
+  regular: 'Roboto-Regular',
+  thin: 'Roboto-Thin',
+};
+
+export default fontFamily;
---
+++ src/styles/sizes.ts
@@ -0,0 +1,32 @@
+import {Dimensions} from 'react-native';
+
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
+
+const height = screenHeight < screenWidth ? screenWidth : screenHeight;
+const width = screenWidth < screenHeight ? screenWidth : screenHeight;
+
+const guidelineBaseWidth = 360;
+const guidelineBaseHeight = 680;
+
+function percentageWidth(percent: number): number {
+  return width * (percent / 100);
+}
+
+function percentageHeight(percent: number): number {
+  return height * (percent / 100);
+}
+
+const horizontalScale = (size: number) => (width / guidelineBaseWidth) * size;
+const verticalScale = (size: number) => (height / guidelineBaseHeight) * size;
+
+const moderateScale = ({size, factor = 0.5}: {size: number; factor?: number}) =>
+  size + (horizontalScale(size) - size) * factor;
+
+export {
+  percentageHeight,
+  percentageWidth,
+  horizontalScale,
+  verticalScale,
+  moderateScale,
+};
