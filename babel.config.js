9a529c621189cbec29ebfd3b8949ea434b8ebc37
Twin Edo <twinedo.dev@gmail.com>
2024-06-24 01:21:41 +0700

    babel configjs

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,19 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    ["module-resolver", {
-      root: ['./src'],
-      extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-      alias: {
-        tests: ['./tests'],
-        '@assets': './src/assets',
-        '@components': './src/components',
-        '@styles': './src/styles',
-        '@utils': './src/utils',
-      },
-    }]
-  ]
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      ["module-resolver", {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests'],
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+        },
+      }]
+    ]
+  }
 };
